00c674b057982a0c289ba22773d4595c523b6263
flubbid <brendanwilson1@gmail.com>
2019-06-20 16:33:02 -0500

    adds urls paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('assignments/', views.ass_index, name='ass_index'),
     path('assignments/<int:ass_id>/', views.ass_detail, name='detail'),
-    path('assignments/create/', views.AssCreate.as_view(), name='cats_create'),
-    path('')
+    path('assignments/create/', views.AssCreate.as_view(), name='ass_create'),
 ]
