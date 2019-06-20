9154d84419add6ee7a75352cd1422b5500455f26
flubbid <brendanwilson1@gmail.com>
2019-06-20 16:33:35 -0500

    corrects view issues, by not adding @login_required to the class views

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Teacher, Student, Assignment
 # Create your views here.
 def signup(request):
     error_message = ''
-    if reqeuest.method == 'POST'
-    form = UserCreationForm(request.POST)
-    if form.is.valid():
-        user = form.save()
-        login(request, user)
-        return redirect('index')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
         else:
             error_message = 'You put the incorrect credentials TRY AGAIN!'
-        form = UserCreationForm()
-        context = {'form': form, 'error_message': error_message}
+            form = UserCreationForm()
+            context = {'form': form, 'error_message': error_message}
         return render(request, 'registration/signup.html', context)
 
-@login_required 
-class AssCreate(CreateView):
+
+class AssCreate(LoginRequiredMixin, CreateView):
     model = Assignment
-    fields = ['title', 'description', 'due_date']
+    fields = ['name', 'description', 'due_date']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
 
 class AssUpdate(UpdateView):
     model = Assignment
@@ -32,8 +43,8 @@ def home(request):
     return render(request, 'home.html')
 
 def ass_index(request):
-    assignments = Assignment.objects.all()
-    return render (request, 'assignment/index.html', 'assignments': assignments)
+    assignments = Assignment.objects.all(user = request.user)
+    return render (request, 'assignment/index.html', {'assignments': assignments})
 
 def ass_detail(request, ass_id):
     ass = Assignment.objects.get(id=ass_id)
