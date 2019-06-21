25b0aae54d560c648259f7132f202536f6886bfd
flubbid <brendanwilson1@gmail.com>
2019-06-21 00:09:05 -0500

    adds updates to login path and changes assignment paths per folder name change

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login_view'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup, name='signup'),
     # path('students/create', views.StudentCreate.as_view(), name='stud_create'),
     # path('students/', views.student_index, name='stud_index'),
-    path('assignments/', views.ass_index, name='ass_index'),
-    path('assignments/<int:assignment_id>/', views.ass_detail, name='detail'),
-    path('assignments/create/', views.AssCreate.as_view(), name='ass_create'),
+    path('Assignments/', views.ass_index, name='ass_index'),
+    path('Assignments/<int:assignment_id>/', views.ass_detail, name='detail'),
+    path('Assignments/create/', views.AssCreate.as_view(), name='ass_create'),
 ]
