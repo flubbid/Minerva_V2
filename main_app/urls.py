566cd944121ae0f881ba5336114e551800d29424
flubbid <brendanwilson1@gmail.com>
2019-06-21 03:26:28 -0500

    fixed the ass detail link to show correct id

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login_view, name='login_view'),
-    path('logout/', views.logout_view, name='logout'),
-    path('signup/', views.signup, name='signup'),
     # path('students/create', views.StudentCreate.as_view(), name='stud_create'),
     # path('students/', views.student_index, name='stud_index'),
     path('Assignments/', views.ass_index, name='ass_index'),
-    path('Assignments/<int:assignment_id>/', views.ass_detail, name='detail'),
+    path('Assignments/<int:dog_id>/', views.ass_detail, name='detail'),
     path('Assignments/create/', views.AssCreate.as_view(), name='ass_create'),
+    path('accounts/login/', views.login_view, name='login_view'),
+    path('accounts/logout/', views.logout_view, name='logout'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
