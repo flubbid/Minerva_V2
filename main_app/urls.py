3e6eb64688ea09fed6d1063eb228494de8519dab
flubbid <brendanwilson1@gmail.com>
2019-06-20 23:25:54 -0500

    adds correct path to login

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login_view'),
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
     # path('students/create', views.StudentCreate.as_view(), name='stud_create'),
